f7486c1cccf5eb625116abdff96c16909f0025e8
omkarmahasuar99 <126793930+omkarmahasuar99@users.noreply.github.com>
2023-04-17 11:25:07 +0530

    factorial of n

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Please enter a number");
+        int n = sc.nextInt();
+        int fact=1;
+        for(int i=n;i>0;i--)
+        {
+           fact=fact*i;
+        }System.out.println("The factorial of "+n+" is "+fact);
+    }
+}
